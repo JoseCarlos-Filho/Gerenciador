66b0d5ca904716638fd7840ecacbc7d6d3261c6f
JoseCarlos-Filho <etijcvivi@gmail.com>
2023-05-08 22:30:19 -0300

    feat: utilizando RequestDispatcher para fazer uma chamada a um JSP.

--- src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -3,6 +3,7 @@ package br.com.alura.gerenciador.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -31,8 +32,10 @@ public class NovaEmpresaServlet extends HttpServlet {
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
 
-		PrintWriter out = response.getWriter();
-		out.println("<html><body><h1>Empresa " + nomeEmpresa + " cadastrada com sucesso!</h1></body></html>");
+		// chamar o JSP
+		RequestDispatcher requisicao = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+		request.setAttribute("empresa", empresa.getNome());
+		requisicao.forward(request, response);
 	}
 
 }
