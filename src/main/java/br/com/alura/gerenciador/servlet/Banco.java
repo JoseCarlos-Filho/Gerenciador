8a3286b79826d154d41efcc6bd03785abac4ae21
JoseCarlos-Filho <etijcvivi@gmail.com>
2023-05-15 11:16:15 -0300

    refactor: criando id para a maniulação dos dados da empresa onde cada empresa recebe seu id.

--- src/main/java/br/com/alura/gerenciador/servlet/Banco.java
+++ src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -1,15 +1,19 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
 
 	private static List<Empresa> listaEmpresa = new ArrayList<>();
+	private static Integer chaveSequencial = 1;
 	
 	static {
 		Empresa empresa1 = new Empresa();
+		empresa1.setId(chaveSequencial++);
 		Empresa empresa2 = new Empresa();
+		empresa2.setId(chaveSequencial++);
 		
 		empresa1.setNome("JF");
 		empresa2.setNome("JCR");
@@ -20,11 +24,43 @@ public class Banco {
 	}
 	
 	public void adiciona(Empresa empresa) {
+		empresa.setId(Banco.chaveSequencial++);
 		Banco.listaEmpresa.add(empresa);
 	}
 	
 	public List<Empresa> getEmpresas() {
 		return Banco.listaEmpresa;
+	}
+
+	public void removeEmpresa(Integer id) {
+		
+		Iterator<Empresa> it = listaEmpresa.iterator();
+		
+		while(it.hasNext()) {
+			Empresa emp = it.next();
+			if(emp.getId() == id) {
+				it.remove();
+				System.out.println("Empresa removida!");
+			}
+			
+		}
+		
+//		for (Empresa empresa : listaEmpresa) {
+//			if(empresa.getId() == id) {
+//				listaEmpresa.remove(empresa);
+//				System.out.println("Empresa removida!");
+//			}
+//		}
+		
+	}
+
+	public Empresa buscaEmpresaPorId(Integer id) {
+		for (Empresa empresa : listaEmpresa) {
+			if(empresa.getId() == id) {
+				return empresa;
+			}
+		}
+		return null;
 	}
 
 }
